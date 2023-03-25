6962512eb21079d4875cacb6ef5423a28c720d29
Ayush Jha <ayush@wasmer.io>
2023-03-26 00:01:57 +0545

    add logic for wai bindgen

---
+++ src/bindings.rs
@@ -0,0 +1,11 @@
+use wai_bindgen_rust;
+
+wai_bindgen_rust::export!("polyvalid.wai");
+
+pub struct Polyvalid;
+
+impl polyvalid::Polyvalid for Polyvalid {
+    fn is_name_valid(input_string: String) -> bool {
+        crate::is_name_valid(input_string)
+    }
+}
